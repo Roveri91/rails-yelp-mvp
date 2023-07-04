7a61de095aac34071d4a9b28ce0d51183ce08912
Simone Roveri <spikeroveri91@gmail.com>
2023-07-04 22:05:33 +0900

    working on the view

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,13 +1,2 @@
 class RestaurantsController < ApplicationController
-  def name
-  end
-
-  def address
-  end
-
-  def phone_number
-  end
-
-  def rating:integer
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'restaurants/name'
-  get 'restaurants/address'
-  get 'restaurants/phone_number'
-  get 'restaurants/rating:integer'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
